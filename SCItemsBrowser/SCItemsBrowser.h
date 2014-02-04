08bb3b96d1767198fc51bbdf2b701a66b3a795ef
dodikk <dodikk88.reg@gmail.com>
2014-02-04 17:24:01 +0200

    Initial Commit

---
+++ SCItemsBrowser/SCItemsBrowser.h
@@ -0,0 +1,13 @@
+//
+//  SCItemsBrowser.h
+//  SCItemsBrowser
+//
+//  Created by Oleksandr Dodatko on 2/4/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCItemsBrowser : NSObject
+
+@end
---
+++ SCItemsBrowser/SCItemsBrowser.m
@@ -0,0 +1,13 @@
+//
+//  SCItemsBrowser.m
+//  SCItemsBrowser
+//
+//  Created by Oleksandr Dodatko on 2/4/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import "SCItemsBrowser.h"
+
+@implementation SCItemsBrowser
+
+@end
