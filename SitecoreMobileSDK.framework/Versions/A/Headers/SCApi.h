a6d2938acafdb71939e86a46a350e32d7571ba18
dodikk <dodikk88.reg@gmail.com>
2014-02-04 18:35:02 +0200

    Added a binary of MobileSDK 1.2

---
+++ SitecoreMobileSDK.framework/Versions/A/Headers/SCApi.h
@@ -0,0 +1,43 @@
+//
+//  SCApi.h
+//  SCApi
+//
+//  Created on 1/31/2012.
+//  Copyright 2012. Sitecore. All rights reserved.
+//
+
+#import <SitecoreMobileSDK/SCItemSource.h>
+#import <SitecoreMobileSDK/SCItemSourcePOD.h>
+
+#import <SitecoreMobileSDK/SCApiContext.h>
+#import <SitecoreMobileSDK/SCExtendedApiContext.h>
+
+#import <SitecoreMobileSDK/SCItem.h>
+#import <SitecoreMobileSDK/SCPagedItems.h>
+
+#import <SitecoreMobileSDK/SCBaseItemRequest.h>
+#import <SitecoreMobileSDK/SCEditItemsRequest.h>
+#import <SitecoreMobileSDK/SCItemsReaderRequest.h>
+#import <SitecoreMobileSDK/SCHTMLReaderRequest.h>
+#import <SitecoreMobileSDK/SCCreateItemRequest.h>
+#import <SitecoreMobileSDK/SCCreateMediaItemRequest.h>
+#import <SitecoreMobileSDK/SCPageEventTriggeringRequest.h>
+#import <SitecoreMobileSDK/SCCampaignTriggeringRequest.h>
+
+#import <SitecoreMobileSDK/SCField.h>
+#import <SitecoreMobileSDK/SCDateField.h>
+#import <SitecoreMobileSDK/SCImageField.h>
+#import <SitecoreMobileSDK/SCDateTimeField.h>
+#import <SitecoreMobileSDK/SCCheckboxField.h>
+#import <SitecoreMobileSDK/SCChecklistField.h>
+#import <SitecoreMobileSDK/SCMultilistField.h>
+#import <SitecoreMobileSDK/SCTreelistField.h>
+#import <SitecoreMobileSDK/SCColorPickerField.h>
+#import <SitecoreMobileSDK/SCDroptreeField.h>
+#import <SitecoreMobileSDK/SCFieldLinkData.h>
+#import <SitecoreMobileSDK/SCGeneralLinkField.h>
+
+#import <SitecoreMobileSDK/SCParams.h>
+#import <SitecoreMobileSDK/SCFieldImageParams.h>
+
+#import <SitecoreMobileSDK/SCError.h>
