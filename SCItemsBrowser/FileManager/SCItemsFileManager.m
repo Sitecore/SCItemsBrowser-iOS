cba771bb4cea6c69b95021d6830724c084bc12a1
dodikk <dodikk88.reg@gmail.com>
2014-02-05 18:12:17 +0200

    Implemented a constructor

--- SCItemsBrowser/FileManager/SCItemsFileManager.m
+++ SCItemsBrowser/FileManager/SCItemsFileManager.m
@@ -1,4 +1,34 @@
 #import "SCItemsFileManager.h"
 
 @implementation SCItemsFileManager
+{
+    SCExtendedApiContext*          _apiContext;
+    id<SCItemsLevelRequestBuilder> _nextLevelRequestBuilder;
+}
+
+-(instancetype)init
+{
+    [ self doesNotRecognizeSelector: _cmd ];
+    return nil;
+}
+
+-(instancetype)initWithApiContext:( SCExtendedApiContext* )apiContext
+              levelRequestBuilder:( id<SCItemsLevelRequestBuilder> )nextLevelRequestBuilder
+{
+    NSParameterAssert( nil != apiContext );
+    NSParameterAssert( nil != nextLevelRequestBuilder );
+    
+    self = [ super init ];
+    
+    if ( nil == self )
+    {
+        return nil;
+    }
+    
+    self->_apiContext = apiContext;
+    self->_nextLevelRequestBuilder = nextLevelRequestBuilder;
+    
+    return nil;
+}
+
 @end
