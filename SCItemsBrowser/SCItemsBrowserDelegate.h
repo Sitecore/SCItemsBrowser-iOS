3f1ebd8bd897aca2ce0003f37b0ceed0a2765f3a
dodikk <dodikk88.reg@gmail.com>
2014-02-11 17:02:13 +0200

    Added delegate methods

--- SCItemsBrowser/SCItemsBrowserDelegate.h
+++ SCItemsBrowser/SCItemsBrowserDelegate.h
@@ -1,5 +1,13 @@
 #import <Foundation/Foundation.h>
 
+@protocol SCUploadProgress;
+
 @protocol SCItemsBrowserDelegate <NSObject>
+
+-(void)itemsBrowser:( id )sender
+didReceiveLevelProgressNotification:( id<SCUploadProgress> )progressInfo;
+
+-(void)itemsBrowser:( id )sender
+levelLoadinFailedWithError:( NSError* )error;
 
 @end
