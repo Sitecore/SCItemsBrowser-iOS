9acb94215a430e50b8860f7470b570b79ff2b96b
dodikk <dodikk88.reg@gmail.com>
2014-02-12 12:31:19 +0200

    Added outlets and stub methods for protocols

--- ManualTests/ListItemsBrowserDemo/ListItemsBrowserDemo/LBViewController.m
+++ ManualTests/ListItemsBrowserDemo/ListItemsBrowserDemo/LBViewController.m
@@ -1,29 +1,92 @@
-//
-//  LBViewController.m
-//  ListItemsBrowserDemo
-//
-//  Created by Oleksandr Dodatko on 2/11/14.
-//  Copyright (c) 2014 Sitecore. All rights reserved.
-//
-
 #import "LBViewController.h"
 
 @interface LBViewController ()
+<
+    SCItemsBrowserDelegate,
+    SIBListModeAppearance ,
+    SIBListModeCellFactory,
+    SCItemsLevelRequestBuilder
+>
+
+@property (strong, nonatomic) IBOutlet SCItemListBrowser *itemsBrowserController;
+@property (weak, nonatomic) IBOutlet UITextView *itemPathTextView;
 
 @end
 
 @implementation LBViewController
-
-- (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    SCApiContext* _legacyApiContext;
+    SCExtendedApiContext* _apiContext;
 }
 
-- (void)didReceiveMemoryWarning
+-(void)setupContext
 {
-    [super didReceiveMemoryWarning];
+    self->_legacyApiContext = [ SCApiContext contextWithHost: @"http://mobiledev1ua1.dk.sitecore.net:722"
+                                                 login: @"sitecore\\admin"
+                                              password: @"b"
+                                               version: SCWebApiMaxSupportedVersion ];
+
+    self->_legacyApiContext.defaultDatabase = @"master";
+    self->_legacyApiContext.defaultSite = @"/sitecore/shell";
+    
+    self->_apiContext = self->_legacyApiContext.extendedApiContext;
+}
+
+-(void)viewDidLoad
+{
+    [ super viewDidLoad ];
+    
+    NSParameterAssert( nil != self.itemsBrowserController );
+    
+    [ self setupContext ];
+    self.itemsBrowserController.apiContext = self->_apiContext;
+        
+    [ self->_apiContext itemReaderForItemPath: @"/sitecore/content/home"
+                                   itemSource: nil ];
+}
+
+-(void)didReceiveMemoryWarning
+{
+    [ super didReceiveMemoryWarning ];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark -
+#pragma mark SCItemsBrowserDelegate
+-(void)itemsBrowser:( id )sender
+didReceiveLevelProgressNotification:( id<SCUploadProgress> )progressInfo
+{
+    NSAssert( NO, @"not implemented" );
+}
+
+-(void)itemsBrowser:( id )sender
+levelLoadinFailedWithError:( NSError* )error
+{
+    NSAssert( NO, @"not implemented" );    
+}
+
+#pragma mark -
+#pragma mark SIBListModeCellFactory
+-(UITableViewCell*)createLevelUpCellForListMode
+{
+    NSAssert( NO, @"not implemented" );
+    return nil;
+}
+
+-(UITableViewCell<SCItemCell>*)createCellForListMode
+{
+    NSAssert( NO, @"not implemented" );
+    return nil;
+}
+
+
+#pragma mark -
+#pragma mark SCItemsLevelRequestBuilder
+-(SCItemsReaderRequest*)levelDownRequestForItem:( SCItem* )item
+{
+    NSAssert( NO, @"not implemented" );
+    return nil;
 }
 
 @end
