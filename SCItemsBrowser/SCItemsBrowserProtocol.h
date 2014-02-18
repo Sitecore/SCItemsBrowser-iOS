44b056dbe61d3a21caa3519b4d1af37d08e7e327
dodikk <dodikk88.reg@gmail.com>
2014-02-18 21:48:03 +0200

    Added documentation

--- SCItemsBrowser/SCItemsBrowserProtocol.h
+++ SCItemsBrowser/SCItemsBrowserProtocol.h
@@ -1,9 +1,25 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+ This protocol describes data load methods that should be supported by each viewController.
+ */
 @protocol SCItemsBrowserProtocol <NSObject>
 
+/**
+ Reloads items using default ApiContext settings.
+ */
 -(void)reloadData;
+
+/**
+ Reloads items by activating the SCItemReaderRequestIngnoreCache flag.
+ */
 -(void)forceRefreshData;
+
+
+/**
+ Loads child items of the root item using default ApiContext settings.
+ */
 -(void)navigateToRootItem;
 
 @end
