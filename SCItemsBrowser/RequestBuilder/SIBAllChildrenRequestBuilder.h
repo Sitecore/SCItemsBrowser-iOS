afd4bef38766f8280c8bce3833abbe1537454698
dodikk <dodikk88.reg@gmail.com>
2014-02-19 13:42:37 +0200

    [request builder] Added documentation for request builder classes

--- SCItemsBrowser/RequestBuilder/SIBAllChildrenRequestBuilder.h
+++ SCItemsBrowser/RequestBuilder/SIBAllChildrenRequestBuilder.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <SCItemsBrowser/SCItemsLevelRequestBuilder.h>
 
+
+/**
+ Creates the request to fetch all children of the given item.
+ */
 @interface SIBAllChildrenRequestBuilder : NSObject< SCItemsLevelRequestBuilder >
 @end
--- SCItemsBrowser/RequestBuilder/SIBBlackListTemplateRequestBuilder.h
+++ SCItemsBrowser/RequestBuilder/SIBBlackListTemplateRequestBuilder.h
@@ -1,6 +1,23 @@
 #import <Foundation/Foundation.h>
 #import <SCItemsBrowser/RequestBuilder/SIBAbstractTemplateListRequestBuilder.h>
 
-
+/**
+ Creates the request to fetch only those children of the given item having any templates except those on the initializer list.
+ */
 @interface SIBBlackListTemplateRequestBuilder : SIBAbstractTemplateListRequestBuilder
+
+/**
+ Creates a query based request that fetches children of the given item that match none of the template on the list.
+ 
+ @param sender One of the items browser controllers below.
+ * SCItemListBrowser
+ * SCItemGridBrowser
+ 
+ @param item The children list should be defined for it by this class.
+ 
+ @return A request that defines the scope of child items.
+ */
+-(SCItemsReaderRequest*)itemsBrowser:( id )sender
+             levelDownRequestForItem:( SCItem* )item;
+
 @end
