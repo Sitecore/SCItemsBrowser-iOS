7eeb5512711e90ffd2c2782c80d8befbb100559f
dodikk <dodikk88.reg@gmail.com>
2014-02-25 16:41:47 +0200

    [grid demo] Added cell construction logic

--- ManualTests/GridItemsBrowserDemo/GridItemsBrowserDemo/GBViewController.m
+++ ManualTests/GridItemsBrowserDemo/GridItemsBrowserDemo/GBViewController.m
@@ -191,17 +191,70 @@ shouldLoadLevelForItem:( SCItem* )levelParentItem
 
 #pragma mark -
 #pragma mark SIBGridModeCellFactory
+static NSString* const LEVEL_UP_CELL_ID  = @"net.sitecore.MobileSdk.ItemsBrowser.list.LevelUpCell"   ;
+static NSString* const ITEM_CELL_ID      = @"net.sitecore.MobileSdk.ItemsBrowser.list.ItemCell"      ;
+static NSString* const IMAGE_CELL_ID     = @"net.sitecore.MobileSdk.ItemsBrowser.list.ItemCell.image";
+
+-(NSString*)levelUpCellReuseId
+{
+    return LEVEL_UP_CELL_ID;
+}
+
+-(NSString*)reuseIdentifierForItem:( SCItem* )item
+{
+    if ( [ item isMediaImage ] )
+    {
+        return IMAGE_CELL_ID;
+    }
+    else
+    {
+        return ITEM_CELL_ID;
+    }
+}
+
+-(Class)levelUpCellClass
+{
+    return Nil;
+}
+
+-(Class)cellClassForItem:( SCItem* )item
+{
+    return Nil;
+}
+
 -(UICollectionViewCell*)itemsBrowser:( SCItemGridBrowser* )sender
         createLevelUpCellAtIndexPath:( NSIndexPath* )indexPath
 {
-    return nil;
+    NSString* reuseId = [ self levelUpCellReuseId ];
+    Class levelUpCellClass = [ self levelUpCellClass ];
+    
+    UICollectionView* collectionView = self->_itemsBrowserController.collectionView;
+    [ collectionView registerClass: levelUpCellClass
+        forCellWithReuseIdentifier: reuseId ];
+    
+    UICollectionViewCell* result =
+    [ collectionView dequeueReusableCellWithReuseIdentifier: reuseId
+                                               forIndexPath: indexPath ];
+    
+    return result;
 }
 
 -(UICollectionViewCell<SCItemCell>*)itemsBrowser:( SCItemGridBrowser* )sender
                        createGridModeCellForItem:( SCItem* )item
                                      atIndexPath:( NSIndexPath* )indexPath
 {
-    return nil;
+    NSString* reuseId = [ self reuseIdentifierForItem: item ];
+    Class levelUpCellClass = [ self cellClassForItem: item ];
+    
+    UICollectionView* collectionView = self->_itemsBrowserController.collectionView;
+    [ collectionView registerClass: levelUpCellClass
+        forCellWithReuseIdentifier: reuseId ];
+    
+    UICollectionViewCell<SCItemCell>* result =
+    [ collectionView dequeueReusableCellWithReuseIdentifier: reuseId
+                                               forIndexPath: indexPath ];
+    
+    return result;
 }
 
 @end
