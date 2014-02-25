503e6faadf841b483e2f1f206b9e2acb8ed6410a
dodikk <dodikk88.reg@gmail.com>
2014-02-25 15:19:24 +0200

    [Grid demo] Renamed itemsBrowser controller

--- ManualTests/GridItemsBrowserDemo/GridItemsBrowserDemo/GBViewController.m
+++ ManualTests/GridItemsBrowserDemo/GridItemsBrowserDemo/GBViewController.m
@@ -2,24 +2,110 @@
 
 @interface GBViewController ()<SCItemsBrowserDelegate, SIBGridModeAppearance, SIBGridModeCellFactory>
 
-@property (strong, nonatomic) IBOutlet SCItemGridBrowser *itemsBrowser;
-@property (strong, nonatomic) IBOutlet SIBAllChildrenRequestBuilder *requestBuilder;
-
+@property (strong, nonatomic) IBOutlet SCItemGridBrowser *itemsBrowserController;
+@property (strong, nonatomic) IBOutlet SIBAllChildrenRequestBuilder *allChildrenRequestBuilder;
 
 @end
 
 @implementation GBViewController
+{
+    SCApiContext* _legacyApiContext;
+    SCExtendedApiContext* _apiContext;
+}
+
+-(void)setupContext
+{
+    self->_legacyApiContext =
+    [ SCApiContext contextWithHost: @"http://mobiledev1ua1.dk.sitecore.net:7200"
+                             login: @"sitecore\\admin"
+                          password: @"b"
+                           version: SCWebApiMaxSupportedVersion ];
+    
+    self->_legacyApiContext.defaultDatabase = @"master";
+    self->_legacyApiContext.defaultSite = @"/sitecore/shell";
+    
+    self->_apiContext = self->_legacyApiContext.extendedApiContext;
+}
 
 -(void)viewDidLoad
 {
     [ super viewDidLoad ];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSParameterAssert( nil != self.itemsBrowserController );
+    
+    [ self setupContext ];
+    self.itemsBrowserController.apiContext = self->_apiContext;
+    
+    
+    SCExtendedAsyncOp rootItemLoader =
+    [ self->_apiContext itemReaderForItemPath: ROOT_ITEM_PATH
+                                   itemSource: nil ];
+    
+    [ self startLoading ];
+    __weak GBViewController* weakSelf = self;
+    rootItemLoader( nil, nil, ^( SCItem* rootItem, NSError* blockError )
+                   {
+                       [ weakSelf endLoading ];
+                       
+                       if ( nil == rootItem )
+                       {
+                           [ weakSelf didFailLoadingRootItemWithError: blockError ];
+                       }
+                       else
+                       {
+                           [ weakSelf didLoadRootItem: rootItem ];
+                       }
+                   } );
 }
 
 -(void)didReceiveMemoryWarning
 {
     [ super didReceiveMemoryWarning ];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark -
+#pragma mark SCItemsBrowserDelegate
+-(void)itemsBrowser:( id )sender
+didReceiveLevelProgressNotification:( id )progressInfo
+{
+    NSAssert( NO, @"NOT IMPLEMENTED" );
+}
+
+-(void)itemsBrowser:( id )sender
+levelLoadingFailedWithError:( NSError* )error
+{
+    NSAssert( NO, @"NOT IMPLEMENTED" );
+}
+
+-(void)itemsBrowser:( id )sender
+didLoadLevelForItem:( SCItem* )levelParentItem
+{
+    NSAssert( NO, @"NOT IMPLEMENTED" );
+}
+
+-(BOOL)itemsBrowser:( id )sender
+shouldLoadLevelForItem:( SCItem* )levelParentItem
+{
+    NSAssert( NO, @"NOT IMPLEMENTED" );
+    return NO;
+}
+
+
+#pragma mark -
+#pragma mark SIBGridModeCellFactory
+-(UICollectionViewCell*)itemsBrowser:( SCItemGridBrowser* )sender
+        createLevelUpCellAtIndexPath:( NSIndexPath* )indexPath
+{
+    return nil;
+}
+
+-(UICollectionViewCell<SCItemCell>*)itemsBrowser:( SCItemGridBrowser* )sender
+                       createGridModeCellForItem:( SCItem* )item
+                                     atIndexPath:( NSIndexPath* )indexPath
+{
+    return nil;
 }
 
 @end
