1cd0441393a1b57639c2ddf5468099496b0eef3e
dodikk <dodikk88.reg@gmail.com>
2014-02-28 10:00:43 +0200

    [doc] Added documentation for SCLevelUpItem

--- SCItemsBrowser/ItemsBrowser/SCLevelUpItem.h
+++ SCItemsBrowser/ItemsBrowser/SCLevelUpItem.h
@@ -1,4 +1,11 @@
 #import <Foundation/Foundation.h>
 
+/**
+ A placeholder item that is added to the response dataset if the level is not a root one.
+ Note : the dataset can contain only SCItem and SCLevelUpItem objects.
+ 
+ It is handled differently both from the visual and from the logical perspective.
+ As a user you should not create this class directly. You may need it only when sub-classing SCAbstractItemsBrowser.
+ */
 @interface SCLevelUpItem : NSObject
 @end
